04661218424640625f206e82cd34216da076d7c1
Benjamin Kowalski <Benjamin.Kowalski.1987@gmail.com>
2020-01-20 22:38:13 +0100

    👨‍💻 Do more in the general settings

--- views/settings/settings-main.js
+++ views/settings/settings-main.js
@@ -1,11 +1,34 @@
-const helpers = require('../../libs/helpers/helpers.js')
-const db = require('../../libs/db/db.js')
-const { render } = require('../../libs/render/render.js')
+const helpers = require('../../libs/helpers/helpers.js'),
+	{ db } = require('../../libs/db/db.js'),
+	{ render } = require('../../libs/render/render.js')
 
 exports.default = async (req, res) => {
+
+	var blog = await db.findOne('blogs', {
+		created_by: req.session.uid
+	})
+
+	if (req.body.a === 'met') {
+
+		const success = await db.update('blogs', blog.id, {
+			title: req.body.title,
+			description: req.body.description,
+			language: req.body.language,
+			timezone: req.body.timezone
+		}).catch(err => {
+			console.log('ERR', err)
+		})
+
+		blog = await db.findOne('blogs', {
+			created_by: req.session.uid
+		})
+	}
+
+
   render(req, res, 'settings', {
     title: 'Settings',
     navActive: 'settings',
-    subview: req.params.subview ? req.params.subview : 'general'
+    subview: req.params.subview ? req.params.subview : 'general',
+    blog
   })
 }
